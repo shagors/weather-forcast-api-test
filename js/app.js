462ff6d91ed3289db29ea8b5de78e87ab8481ba5
shagors <shajjadshagor@gmail.com>
2022-03-02 13:32:29 +0600

    icon fixed and all city shows

--- js/app.js
+++ js/app.js
@@ -18,5 +18,8 @@ const displayTemperature = temperature => {
     setInnerText('city', temperature.name);
     setInnerText('temperature', temperature.main.temp);
     setInnerText('condition', temperature.weather[0].main);
-    console.log(temperature);
+    // set weather icon
+    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+    const imgIcon = document.getElementById('weather-icon');
+    imgIcon.setAttribute('src', url);
 };
