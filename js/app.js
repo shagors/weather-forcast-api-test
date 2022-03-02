a06caf7a439ae13e16262ae8ad0b84226ee81bbb
shagors <shajjadshagor@gmail.com>
2022-03-02 12:09:45 +0600

    fetch link api

--- js/app.js
+++ js/app.js
@@ -3,5 +3,8 @@ const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${API_KEY}`;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
-    console.log(url);
+    
+    fetch(url)
+    .then(res => res.json())
+    .then(data => console.log(data));
 };
